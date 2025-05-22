3c03111cd80d27811df1899d1d5a32d905793651
Sayyed Wasim Hussain Bukhari <wasimhussain3@gmail.com>
2025-05-22 22:53:59 +0200

    I updated the Week 01 work and This is NODEJS/HOMEWORK/WEEK02

--- api/src/index.js
+++ api/src/index.js
@@ -2,17 +2,20 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import knex from "./database_client.js";
-import nestedRouter from "./routers/nested.js";
+import mealRouter from "./routers/mealRouterWeek01.js";
+import meals from "./routers/mealsWeek2.js";
+import reservations from "./routers/reservationsWeek2.js";
 
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 
 const apiRouter = express.Router();
 
-// This nested router example can also be replaced with your own sub-router
-apiRouter.use("/nested", nestedRouter);
+apiRouter.use("/meals-week-01", mealRouter);
+apiRouter.use("/meals", meals);
+apiRouter.use("/reservations", reservations);
 
 app.use("/api", apiRouter);
 
@@ -20,85 +23,4 @@ app.listen(process.env.PORT, () => {
   console.log(`API listening on port ${process.env.PORT}`);
 });
 
-// This is a simple example of how to use Knex.js with Express.js
-app.get("/", (req, res) => {
-  const routes = [
-    { route: "/future-meals" },
-    { route: "/past-meals" },
-    { route: "/all-meals" },
-    { route: "/first-meal" },
-    { route: "/last-meal" },
-  ];
-
-  res.json({
-    message: "Hello! This is the MEAL SHARING APP",
-    routes: routes,
-  });
-});
-
-// ROUTE TO GET FUTURE MEALS. USED KNEX QUERY BUILDER
-app.get("/future-meals", async (req, res) => {
-  try {
-    const future_meals = await knex("meal")
-      .where("when", ">", new Date())
-      .orderBy("when", "asc");
-    res.json(future_meals);
-  } catch (error) {
-    console.error("Error fetching meals:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-// ROUTE TO GET PAST MEALS. USED KNEX QUERY BUILDER
-
-app.get("/past-meals", async (req, res) => {
-  try {
-    const past_meals = await knex("meal")
-      .where("when", "<", new Date())
-      .orderBy("when", "asc");
-    res.json(past_meals);
-  } catch (error) {
-    console.error("Error fetching meals:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-// ROUTE TO GET ALL MEALS SORTED BY ID. USING RAW SQL
-app.get("/all-meals", async (req, res) => {
-  try {
-    const result = await knex.raw("SELECT * FROM meal ORDER BY id ASC");
-    const all_meals = Array.isArray(result[0]) ? result[0] : result.rows;
-    res.json(all_meals);
-  } catch (error) {
-    console.error("Error fetching meals:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-// ROUTE to GET FIRST MEAL IN DB. USING RAW SQL
-app.get("/first-meal", async (req, res) => {
-  try {
-    const result = await knex.raw("SELECT * FROM meal ORDER BY id asc LIMIT 1");
-
-    const first_meal = Array.isArray(result[0]) ? result[0] : result.rows; // handles MySQL and PostgreSQL
-    res.json(first_meal);
-  } catch (error) {
-    console.error("Error executing raw SQL:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-// ROUTE to GET LAST MEAL IN DB
-app.get("/last-meal", async (req, res) => {
-  try {
-    const result = await knex.raw(
-      "SELECT * FROM meal ORDER BY id desc LIMIT 1"
-    );
-
-    const last_meal = Array.isArray(result[0]) ? result[0] : result.rows; // handles MySQL and PostgreSQL
-    res.json(last_meal);
-  } catch (error) {
-    console.error("Error executing raw SQL:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+// I moved all the routes for week01 homework to the mealRouterWeek01.js file
